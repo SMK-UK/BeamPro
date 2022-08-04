e0e6bf3b80d241c0d88fd247ff9e45d4b36e60b5
Sean Keenan <keenan.sean@hotmail.co.uk>
2022-08-04 10:58:44 +0100

    latest version

--- Beam_Profile.py
+++ Beam_Profile.py
@@ -2,6 +2,7 @@
 Sean Keenan, 5th Year MPhys Heriot-Watt University, Edinburgh
 Quantum memories group - QPL Heriot-Watt
 Gaussian Beam Profile Extraction
+
 load image files and read camera position data from designated
 folder to produce gaussian fit in x and y dimensions. Fit data
 can then be plotted against the original image and / or saved
@@ -21,33 +22,35 @@ import fit_funcs as ff
 import sys
 
 # to save data / plots and to show plots set flag = 1
-excel_save = 0
-plot_show = 1
+excel_save = 1
+plot_show = 0
 plot_save = 0
 
 # quick waist calculation - no waist fit / find
 quick_show = 0
 
-''' User input information '''
+''' Folder to draw files from and save data to'''
 
 # directory and folder for images
-dir = 'C:\\Users\\sk88\\Documents\\HWSYS\\Desktop'
-folder = '20220311_606_beam_after_collimator_before_telescope'
-# image laser wavelength(um)
-ff.wavelength = 606e-6
-# chip and pixel size (mm) if known (Hamamatsu 9.6 x 7.68 mm, DMK 7.04 x 
-# mm )
-chip_size = 7.04 # 9.6 # 
+dir = 'C:\\Users\\sk88\\Dropbox (Heriot-Watt University Team)\\RES_EPS_Quantum_Photonics_Lab\\Experiments\\Current Experiments\\Broadband telecom quantum memories\\FP Cavity\\20220726_re_alignment_150mm'
+folder = '339mm'
+# image laser wavelength(mm)
+ff.wavelength = 1550e-6
+# chip and pixel size (mm) if known (Hamamatsu 9.6 x 7.68 mm, DK 1.411cm)
+chip_size = 9.6 #14.11 #
 
-''' Set-up Image file names and processing information '''
-# loops through the images in the specified folder (they should be labelled 1, 2, 3 etc. with
-# the background image given after the relevant beam image. i.e. background images are even,
-# beam images are odd. The file containing the image positions should be labelled 'Distance (mm)'
-# with the distances given in mm.
-#
-# The output file for the beam waist and rayleigh range calulations will be the same as the
-# file containing the distance for each image. Due to compaibility issues this shoul always be
-# of the format .xlsx
+''' 
+Set-up Image file names and processing information
+
+This section loops through the images in the specified folder (they should be labelled 1, 2, 3 etc.) with
+the background image given after the relevant beam image. i.e. background images are even,
+beam images are odd. The file containing the image positions should be labelled 'Distance (mm)'
+with the distances given in mm.
+
+The output file for the beam waist and rayleigh range calulations will be the same as the
+file containing the distance for each image. Due to compaibility issues this shoul always be
+of the format .xlsx
+'''
 
 # set file path
 path = dir + '\\' + folder + '\\'
@@ -78,7 +81,7 @@ if quick_show == 0:
     z_pos = z_pos.dropna()
     # remove duplicate positions
     z = z_pos.drop_duplicates()
-    # reshape array to match other, convert from mm to um
+    # reshape array to match other
     z = np.reshape(z.to_numpy(), len(z))
 
     # warn user if insufficient data to continue
@@ -87,9 +90,12 @@ if quick_show == 0:
     elif len(z) != len(image_list)/2:
         sys.exit('Number of images and z-distance mismatch')
 
-''' Read image and subtract background data '''
-# Reads each image, subtracting the relevant background image in-turn, before fitting the
-# data with a Gaussian in both x and y. 
+''' 
+Read image and subtract background data 
+
+Reads each image, subtracting the relevant background image in-turn, before fitting the
+data with a Gaussian in both x and y. 
+'''
 
 # create empty arrays for new data
 data = np.empty([array_len, imsize[0], imsize[1]])
@@ -134,7 +140,7 @@ scale_err = np.abs(fit_err) * pix_size
 
 # calcualate percent error
 FWHM_err = np.array(scale_err[:, :, 2] / scale_data[:, :, 2])
-# calculate FWHM & 1/e^2 (mm)
+# calculate FWHM & 1/e^2 (um)
 FWHM = 2 * np.sqrt(2 * scale_data[:, :, 2] ** 2 * np.log(2)) 
 FWHM_x = np.reshape(FWHM[:,0], (len(FWHM),1))
 FWHM_xerr = np.reshape(FWHM_err[:, 0] * np.ravel(FWHM_x), (len(FWHM),1))
@@ -145,7 +151,11 @@ e2x_err = np.sqrt(2/np.log(2)) * FWHM_xerr
 over_e2y = np.sqrt(2/np.log(2)) * FWHM_y
 e2y_err = np.sqrt(2/np.log(2)) * FWHM_yerr
 
-''' data outputs - check flags for issues '''
+'''
+Data outputs - check flags for issues 
+
+Outputs plot of all data along with fit
+'''
 
 if quick_show == 0:
 
@@ -155,12 +165,18 @@ if quick_show == 0:
     # calculate beam waist fit for FWHM & 1/e^2 (um)
     xw, xw_err = ff.fithyp(z, np.ravel(FWHM_x), fit_paramsx1)
     yw, yw_err = ff.fithyp(z, np.ravel(FWHM_y), fit_paramsy1)
+    # parameters for fit function (guess waist / z-pos)
+    fit_paramsx2 = np.amin(over_e2x), z[np.argmin(over_e2x)]
+    fit_paramsy2 = np.amin(over_e2y), z[np.argmin(over_e2y)]
+    # calculate beam waist fit for FWHM & 1/e^2 (um)
+    exw, exw_err = ff.fithyp(z, np.ravel(over_e2x), fit_paramsx2)
+    eyw, eyw_err = ff.fithyp(z, np.ravel(over_e2y), fit_paramsy2)
     e2xw = np.sqrt(2/np.log(2)) * xw[0]
     e2yw = np.sqrt(2/np.log(2)) * yw[0]
 
     # calculate rayleigh range (um)
-    zr_x = (np.pi * np.power(xw[0], 2)) / ff.wavelength
-    zr_y = (np.pi * np.power(yw[0], 2)) / ff.wavelength
+    zr_x = (np.pi * xw[0] ** 2) / ff.wavelength
+    zr_y = (np.pi * yw[0] ** 2) / ff.wavelength
     # calculate beam divergence (deg)
     theta_x = (ff.wavelength / (np.pi * xw[0])) * 180/np.pi
     theta_y = (ff.wavelength / (np.pi * yw[0])) * 180/np.pi
@@ -168,8 +184,8 @@ if quick_show == 0:
     # print fit to the command line
     print('x FWHM waist = ' + '%.2f ' % xw[0] + 'p/m ' + '%.2f mm' % xw_err[0] + ' located at ' + '%.2f' % (xw[1])+ ' p/m ' + '%.2f mm' % (xw_err[1]))
     print('y FWHM waist = ' + '%.2f ' % yw[0] + 'p/m ' + '%.2f mm' % yw_err[0] + ' located at ' + '%.2f' % (yw[1])+ ' p/m ' + '%.2f mm' % (yw_err[1]))
-    print('x rayleigh range = ' + '%.2f mm ' % (zr_x) + 'with divergence angle = ' + '%.2f deg' % (theta_x * (180 / np.pi)))
-    print('y rayleigh range = ' + '%.2f mm ' % (zr_y) + 'with divergence angle = ' + '%.2f deg' % (theta_y * (180 / np.pi)))
+    print('x rayleigh range = ' + '%.2f mm ' % zr_x + 'with divergence angle = ' + '%.2f deg' % theta_x)
+    print('y rayleigh range = ' + '%.2f mm ' % zr_y + 'with divergence angle = ' + '%.2f deg' % theta_y)
     print('x 1/e^2 waist = ' + '%.2f ' % e2xw + 'p/m ' + '%.2f mm' % xw_err[0])
     print('y 1/e^2 waist = ' + '%.2f ' % e2yw + 'p/m ' + '%.2f mm' % yw_err[0])
 
@@ -180,14 +196,13 @@ if quick_show == 0:
     df_out = pd.DataFrame(data=data_out, columns=col_name) 
 
     # plot fit to data
-    z_plot = np.linspace(start=np.amin(z)-np.amax(z)*0.25, stop=np.amax(z) * 1.25, num=1000, endpoint=True)
-
+    z_plot = np.linspace(start=-1.5*zr_x, stop=1.5*zr_x, num=1000, endpoint=True) + xw[1]
     # plot beam diameter and waist fit
     fig_1, ax_1 = mp.subplots(nrows=2, ncols=1, sharex='all', sharey='all', constrained_layout=True)
     ax_1[0].errorbar(z, over_e2x, yerr = np.ravel(e2x_err), label='1/e^2 Diameter in x', markersize=5, color='red', fmt='x')
-    ax_1[0].plot(z_plot, ff.hyperbolic(z_plot, e2xw, xw[1]), linestyle='--', label='1/e^2 x Fit', color='red')
+    ax_1[0].plot(z_plot, ff.hyperbolic(z_plot, exw[0], exw[1]), linestyle='--', label='1/e^2 x Fit', color='red')
     ax_1[1].errorbar(z, over_e2y, yerr = np.ravel(e2y_err), label='1/e^2 Diameter in y', markersize=5, color='blue', fmt='x')
-    ax_1[1].plot(z_plot, ff.hyperbolic(z_plot, e2yw, yw[1]), linestyle='--', label='1/e^2 y Fit', color='blue')
+    ax_1[1].plot(z_plot, ff.hyperbolic(z_plot, eyw[0], eyw[1]), linestyle='--', label='1/e^2 y Fit', color='blue')
     ax_1[0].errorbar(z, FWHM_x, yerr = np.ravel(FWHM_xerr), label='FWHM Diameter in y', markersize=5, color='green', fmt='x')
     ax_1[0].plot(z_plot, ff.hyperbolic(z_plot, xw[0], xw[1]), linestyle='--', label='FWHM x Fit', color='green')
     ax_1[1].errorbar(z, FWHM_y, yerr = np.ravel(FWHM_yerr), label='FWHM Diameter in y', markersize=5, color='orange', fmt='x')
@@ -201,7 +216,7 @@ if quick_show == 0:
         ax.grid(True)
 
     if plot_save == 1:
-        fig_1.savefig(fname=path + folder + '_' + 'beam_waist_fit.pdf', quality=95, format='pdf')
+        fig_1.savefig(fname=path + folder + '_' + 'beam_waist_fit.pdf', dpi='figure', format='pdf')
 
 # array for fit to data
     x = np.arange(1, imsize[0] + 1, 1)
@@ -227,8 +242,9 @@ for index in range(array_len):
     ax_fity.plot(ff.gaussian(y, *fit_data[index, 1, :]), y, label='Fit in Y')
     ax_fity.plot(data[index, round(fit_data[index, 0, 1]), :], y, label='Y Data')
     # axis label formatting
-    ax_fitx.set_yticks([0, 50, 100, 150])
-    ax_fity.set_xticks([0, 50, 100, 150])
+    ticks = np.linspace(start=0, stop=np.max(data[index]), num=4)
+    ax_fitx.set_yticks(ticks)
+    ax_fity.set_xticks(ticks)
     ax_2.set_xticks(np.arange(0, imsize[0], 150))
     ax_2.set_yticks(np.arange(0, imsize[1], 150))
     # set limits
@@ -255,10 +271,14 @@ for index in range(array_len):
     ax_fity.legend(loc='best', fontsize=8)
 
     if plot_save == 1:
-        fig_2.savefig(fname=path + folder + '_' + str(img_idx[index]) + 'fit.pdf', quality=95, format='pdf')
+        fig_2.savefig(fname=path + folder + '_' + str(img_idx[index]) + 'fit.pdf', dpi='figure', format='pdf')
 
 if plot_show == 1:
         mp.show()
+
+'''
+Save data to excel
+'''
 
 if excel_save == 1:
     # append new data to original and write to file
--- fit_funcs.py
+++ fit_funcs.py
@@ -7,41 +7,127 @@ from scipy.optimize import curve_fit
 wavelength = 1550e-6
 
 def gaussian(x, height, centre, sigma):
-    '''Generates Gaussian with given parameters'''
+    """
+    Generates Gaussian with given parameters
+    
+    Parameters
+    ----------
+
+    x : 1D array 
+        Positional arguments for gaussian
+    height : 1D array
+        Maximum value of gaussian
+    centre : Single value
+        Centre of Gaussian peak
+    sigma : Single value
+        Width of Gaussian
+
+    Returns
+    -------
+
+    1D array of height values for the positional arguments given in x
+    """
     return height * np.exp(-(np.power(x - centre, 2) / (2 * sigma ** 2)))
 
-def hyperbolic(z, waist, z_0):
-    '''generate hyperbolic function for given beam waist, z positions and wavelength'''  
-    # laser wavelength
+def hyperbolic(z, waist, z_0): 
+    """
+    Generate hyperbolic function for given beam waist, z positions and wavelength
 
-    return waist * np.sqrt(1 + ((((z - z_0) * wavelength)/ (np.pi * waist ** 2)) ** 2))
+    Parameters
+    ----------
+
+    z : 1D array 
+        Positional arguments for hyperbolic 
+    waist : Single Value
+        Minimum y-value for the function
+    z_0 : Single Value
+        Position of waist
+
+    Returns
+    -------
+
+    1D array of intensity values for the given positional arguments in z
+    """
+    return waist * np.sqrt(1 + ((((z - z_0) * wavelength)/(np.pi * waist ** 2)) ** 2))
+
 
 # fit a gaussian to data by calculating its 'moments' (mean, variance, width, height)
 def moments(data):
-    '''Calculates parameters of a gaussian function by calculating
-    its moments (height, mean_x, width_x, mean_y, width_y'''
+    """
+    Calculates parameters of a gaussian function by calculating
+    its moments (height, index_x, width_x, index_y, width_y)
+
+    Parameters
+    ----------
+
+    data : 2D array 
+        2-Dimensional array of intensity data
+
+    Returns
+    -------
+
+    Extracts the following along a single array within the matrix - the
+    x and y values will be calculated along the same index as the maximum value of the matrix
+
+    1D array with the following:
+
+    height : Single value
+        Calculates maximum value of the array and returns the index's to internal function
+    index_x : Single value
+        Calculates index of peak value along one-dimension of the matrix
+    sigma_x : Single value
+        Calculates the width (FWHM) along the first array
+    index_y : Single value
+        Calculates index of peak value along the other dimension of the matrix
+    sigma_y : Single value
+        Calculates the width (FWHM) along the second array
+    """
+
+    # find height and centre of gaussian
     height = np.amax(data)
     centre = np.where(data == height)
     dim = np.size(centre)
     # handle case where there are multiple 'max' values
     if dim > 2:
         centre = np.array(centre)
-        mean = np.sum(centre, 1)
-        mean_x = round(mean[0] / len(centre[0]))
-        mean_y = round(mean[1] / len(centre[1]))
+        index = np.sum(centre, 1)
+        index_x = round(index[0] / len(centre[0]))
+        index_y = round(index[1] / len(centre[1]))
     else:
-        mean_x = int(centre[0])
-        mean_y = int(centre[1])
+        index_x = int(centre[0])
+        index_y = int(centre[1])
 
-    row = data[:, mean_y]
-    col = data[mean_x, :]
+    # extract widths along the 2 dimensions
+    row = data[:, index_y]
+    col = data[index_x, :]
     sigma_x = np.sqrt(((row - height) ** 2).sum() / len(row))
     sigma_y = np.sqrt(((col - height) ** 2).sum() / len(col))
-    return height, mean_x, sigma_x, mean_y, sigma_y
+
+    return height, index_x, sigma_x, index_y, sigma_y
 
 def fitgauss(data):
-    '''Returns seperate x-y Gaussian parameters from fit to 2D gaussian data
-     (height, mean_x, width_x, mean_y, width_y)'''
+    """
+    Returns seperate x-y Gaussian parameters from fit to 2D gaussian data
+    (height, centre_x, width_x, centre_y, width_y)
+
+    Calls to moments(data) in order to extract relevant parameters of the 
+    2D gaussian data before finding the fit to the data. See scipy.optimize.curve_fit
+    for more on data fitting.
+
+    Parameters
+    ----------
+
+    data : 2D array 
+        2-Dimensional array of intensity data
+
+    Returns
+    -------
+
+    fit_data : 1D Array
+        Fitted variables: height, sigma, mean
+    fit_err : 1D Array
+        Uncertainty in fitted variables
+    """
     params = moments(data)
     # extract data along index of maximum value
     x = data[:, params[3]]
@@ -56,9 +142,36 @@ def fitgauss(data):
     fit_err = np.array([x_err, y_err])
     return fit_data, fit_err
 
-def fithyp(z, beam_d, params):
-    ''' Returns the beam waist of an array of beam diameters at positions
-    along z for a fundamental mode gaussian beam '''
-    fit, success = curve_fit(hyperbolic, z, beam_d, p0=params, method='lm')
+def fithyp(z, beam_d, params=None, meth=None, lims=(-np.inf, np.inf)):
+    """
+    Returns the beam waist of an array of beam diameters at positions
+    along z for a fundamental mode gaussian beam
+
+    Parameters
+    ----------
+
+    z : 1D Array
+        Position data corresponding to diameter data in beam_d
+    beam_d : 1D Array
+        Array of beam diameters according to positions in z
+    params : 1D Array
+        Guess values for hyperbolic function; waist, z_0
+    meth : Single string {'lm', 'tf', 'dogbox'}, optional
+        Method to use for optimisation. See 
+        scipy.optimize.curve_fit for details
+    bounds : 2-tuple of array_like, optional
+        Lower and upper bounds on parameters. Defaults to 
+        no bounds. 
+        See scipy.optimize.curve_fit for details
+
+    Returns
+    -------
+
+    fit_data : 1D Array
+        Fitted variables: height, sigma, mean
+    fit_err : 1D Array
+        Uncertainty in fitted variables
+    """
+    fit, success = curve_fit(hyperbolic, z, beam_d, p0=params, method=meth, bounds=lims)
     fit_err = np.sqrt(np.diag(success))
     return fit, fit_err
